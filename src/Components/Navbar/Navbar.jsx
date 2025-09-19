828ae2a66fbdacd582954c15313b4426c690c07e
Virendra Maurya <virendra.maurya@cirrius.com>
2025-09-19 15:39:33 +0530

    Navbar error solve

--- src/Components/Navbar/Navbar.jsx
+++ src/Components/Navbar/Navbar.jsx
@@ -2,7 +2,7 @@ import React, { useContext, useState } from 'react'
 import "./Navbar.css"
 import { assets } from '../../assets/assets'
 import { Link } from 'react-router-dom';
-import { StoreContext } from '../../context/storeContext';
+import { StoreContext } from '../../context/StoreContext';
 const Navbar = ({setShowlogin}) => {
 
 const [menu, setMenu] = useState("home");
