adb44aacf1287250242df6a13ae317145e69f179
Virendra Maurya <virendra.maurya@cirrius.com>
2025-09-19 16:12:12 +0530

    Cart issue solve

--- src/pages/Cart/Cart.jsx
+++ src/pages/Cart/Cart.jsx
@@ -1,5 +1,5 @@
 import React, { useContext } from 'react'
-import { StoreContext } from '../../context/storeContext'
+import { StoreContext } from '../../context/StoreContext'
 import './Cart.css'
 import { useNavigate } from 'react-router-dom';
 const Cart = () => {
--- src/pages/PlaceOrder/PlaceOrder.jsx
+++ src/pages/PlaceOrder/PlaceOrder.jsx
@@ -1,6 +1,6 @@
 import React, { useContext } from 'react'
 import "./PlaceOrder.css"
-import { StoreContext } from '../../context/storeContext'
+import { StoreContext } from '../../context/StoreContext'
 
 const PlaceOrder = () => {
 
